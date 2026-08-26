85cc8d9d69d7f2d38495f478bada359453b02b06
Rina Ivanišević <ivanisevicrina1999@gmail.com>
2026-08-26 13:36:47 +0200

    Implementacija odjela dizajna i logistike

---
+++ src/main/java/hr/tvz/kanban/engine/DesignDepartment.java
@@ -0,0 +1,21 @@
+package hr.tvz.kanban.engine;
+
+import hr.tvz.kanban.model.ActionResult;
+import hr.tvz.kanban.model.DepartmentType;
+import hr.tvz.kanban.model.GameState;
+import hr.tvz.kanban.model.Player;
+
+public class DesignDepartment extends Department{
+    protected DesignDepartment(String name, DepartmentType type) {
+        super("Dizajn", DepartmentType.DESIGN);
+    }
+
+    @Override
+    public ActionResult performAction(Player player, GameState gameState) {
+       player.setCurrentDepartment(DepartmentType.DESIGN);
+       player.addDesignPoints();
+
+       String message = player.getName() + "dobio je jedan bod za dizajn";
+       return new ActionResult(true, message);
+    }
+}
---
+++ src/main/java/hr/tvz/kanban/engine/LogisticsDepartment.java
@@ -0,0 +1,20 @@
+package hr.tvz.kanban.engine;
+
+import hr.tvz.kanban.model.ActionResult;
+import hr.tvz.kanban.model.DepartmentType;
+import hr.tvz.kanban.model.GameState;
+import hr.tvz.kanban.model.Player;
+
+public class LogisticsDepartment extends Department{
+    protected LogisticsDepartment(String name, DepartmentType type) {
+        super("Logistika", DepartmentType.LOGISTICS);
+    }
+
+    @Override
+    public ActionResult performAction(Player player, GameState gameState) {
+        player.setCurrentDepartment(DepartmentType.LOGISTICS);
+        player.addComponent();
+        String message = player.getName() + " je uzeo jednu komponentu";
+        return new ActionResult(true, message);
+    }
+}
